e2667cfdc6a50db9f1d4773191f576c9bfaadfc7
eyob2m <eyob2etemariam@gamil.com>
2023-08-17 17:35:47 +0300

    inistall

--- page/page.jsx
+++ page/page.jsx
@@ -1,24 +1,79 @@
 import './page.css'
-import ppic from './pic.jpg'
+import ppic from './pic7.png'
 import js from '../src/assets/js.jpg'
 import react from '../src/assets/react.jpg'
 import git from '../src/assets/git.jpg'
 import html from '../src/assets/html.jpg'
 import css from '../src/assets/css.jpg'
-import {AiOutlineGithub,AiOutlineInstagram,AiOutlineLinkedin, AiOutlinePhone, AiOutlineMail } from "react-icons/ai";
+import { AiOutlineClose } from 'react-icons/ai'
+import { LiaUniversitySolid } from 'react-icons/lia'
+import {AiOutlineGithub,  AiOutlineInstagram,AiOutlineLinkedin, AiOutlinePhone, AiOutlineMail } from "react-icons/ai";
 import { LiaTelegramPlane } from "react-icons/lia";
+import { useState } from 'react'
 export default function Page() {
+
+function subFun(e) {
+  e.preventDefault();  setTimeout( async ()=> {  document.querySelectorAll('.submitBtn')[1].innerHTML = await "Submit"},5000);  document.querySelectorAll('.submitBtn')[1].innerHTML = "Message sent, I will Respond ASAP"; }
+
+function subFun2(e) {
+   e.preventDefault();  setTimeout( async ()=> {  document.querySelectorAll('.submitBtn')[0].innerHTML = await "Submit"},5000);  document.querySelectorAll('.submitBtn')[0].innerHTML = "Message sent, I will Respond ASAP"; }
+
+
+const [r,setr] = useState(true)
+
+function resumec() {
+if(r) {
+  document.querySelector('.resume').classList.add('dnone') ; 
+
+
+  setr(!r) }
+  if(!r) {
+    document.querySelector('.resume').classList.remove('dnone'); setr(!r) }
+  
+
+}
+  
+
+function skF(e) {
+  let c= ''
+switch(e.target.alt) {
+
+  case "Html Pic": c = 'addhtml'; break;
+  case "Js Pic": c = 'addjs'; break;
+  case "Css Pic": c = 'addcss'; break;
+  case "Git Pic": c = 'addgit'; break;
+  case "React Pic": c = 'addreact'; break; }
+  document.querySelector('.skillWrapper').classList.add(c)
+
+
+}
+
+function skG(e) {
+
+  let c= ''
+  switch(e.target.alt) {
+  
+  
+    case "Html Pic": c = 'addhtml'; break;
+    case "Js Pic": c = 'addjs'; break;
+    case "Css Pic": c = 'addcss'; break;
+    case "Git Pic": c = 'addgit'; break;
+    case "React Pic": c = 'addreact'; break; }
+    document.querySelector('.skillWrapper').classList.remove(c)
+  
+  
+}
   return (
   <>
    <div className="nav">
 <div className="navWrapper">
- <span>Resume</span> 
+ <span onClick={resumec} className='resume0' >Resume</span> 
 </div>
    </div>
 
 <div className="home">
 <div className="homeWrapper">
-<img src={ppic} alt="" />
+<img className='pp' src={ppic} alt="" />
 
 <div className="right">
   
@@ -27,7 +82,7 @@ export default function Page() {
   <span>Frontend Developer</span>
    <p>Im Currently software engineering student at aastu interested in working as freelance , parttime and full time frontend developer
    </p> 
-   <button className='btn1'>Download CV</button>
+   <a download href="../src/assets/robbins.pdf"> <button className='btn1' >Download CV</button></a>
    </div>
   
  </div>
@@ -35,15 +90,40 @@ export default function Page() {
 </div>
 
 
+<div className="about">
+<h2 className='titleA'>About</h2>
+  <div className='aboutWrapper'>
+  <div className="whoAmI">
+  <b>Who I am</b>
+<p>
+Im Currently software engineering student at aastu interested in working as freelance , parttime and full time frontend developer Im Currently software engineering student at aastu interested in working as freelance , parttime and full time frontend developer
+</p>
+</div>
+<div className="edu">
+<b>Education</b>
+<p>
+<LiaUniversitySolid className='uniLogo' /> Addis Ababa Science And Technology University <br />
+Software Engineering<br />
+2020-2025<br />
+<LiaUniversitySolid className='uniLogo' />Entoto Poly Technic Colledge <br />
+Basic Electronics and Communication Multimedia<br />
+2017-2019
+</p> 
+</div>
+</div>
+</div>
+
+
 
 <div className="skill">
 <div className="skillWrapper">
+<h2 className="titleS">Skills</h2>
 <ul>
-  <li> <span>Html</span> <img src={html} alt="Html Pic" /> </li>
-  <li> <span>Css</span><img src={css} alt="Css Pic" /></li>
-  <li> <span>Javascript</span><img src={js} alt="Js Pic" /></li>
-  <li> <span>React</span><img  src={react} alt="React Pic" /></li>
-  <li> <span>Git / Github</span><img src={git} alt="Git" /></li>
+  <li>  <span>Html</span> <img  onMouseLeave={skG} onMouseEnter={skF} src={html} alt="Html Pic" /> </li>
+  <li> <span>Css</span><img onMouseLeave={skG} onMouseEnter={skF} src={css} alt="Css Pic" /></li>
+  <li> <span>Javascript</span><img onMouseLeave={skG} onMouseEnter={skF} src={js} alt="Js Pic" /></li>
+  <li> <span>React</span><img onMouseLeave={skG} onMouseEnter={skF} src={react} alt="React Pic" /></li>
+  <li> <span>Git / Github</span><img onMouseLeave={skG} onMouseEnter={skF} src={git} alt="Git Pic" /></li>
 </ul>
 </div>
 </div>
@@ -52,22 +132,24 @@ export default function Page() {
 
 
 <div className="contact">
-<h2 className='mobileC'>Contact</h2>
 <div className="contactWrapper">
+<div className="titleC">Contact</div>
 <div className="left">
-<h2>Contact</h2>
-<h3 className='emailAdd' > <AiOutlinePhone />  eyob2etemariam@gmail.com</h3>
-<h3 className='phoneAdd'> <AiOutlineMail /> +251985238243</h3>
+<h3 className='emailAdd' > <AiOutlinePhone className='phoneLogo'/>  eyob2etemariam@gmail.com</h3>
+<h3 className='phoneAdd'> <AiOutlineMail className='mailLogo' /> +251985238243</h3>
 <div className="linksList">  
-<AiOutlineGithub /> <AiOutlineInstagram /> <AiOutlineLinkedin /> <LiaTelegramPlane />
+<a href='g.com' className=' logos gitLogo' > <AiOutlineGithub  />     </a>
+<a href='g.com' className=' logos instaLogo' >  <AiOutlineInstagram /></a>
+<a href='g.com' className=' logos linkLogo' >  <AiOutlineLinkedin /></a>
+<a href='g.com' className=' logos teleLogo' >   <LiaTelegramPlane /></a>
 
 </div>
-<button type='submit' className='btn1' >Download Cv</button>
+ <a download href="../src/assets/robbins.pdf"> <button className='btn1' >Download CV</button></a>
 </div>
-<form action="#">
+<form  onSubmit={subFun2}  action="#">
 <input className='nameInput' placeholder='Name' type="text" name='subject'/>
 <input className='emailInput' placeholder='Email' type="email" name='email' />
-<textarea className='msgInput' placeholder='Your Message' name="msg" id="" cols="50"  rows="10"></textarea>
+<textarea className='msgInput' placeholder='Your Message' name="msg" id="" cols="20"  rows="10"></textarea>
 <button className='submitBtn' >Submit</button>
 
 
@@ -78,6 +160,18 @@ export default function Page() {
 
 </div>
 
+<div className="resume">
+<form onSubmit={subFun}  action="#">
+<input className=' in nameInput' placeholder='Name' type="text" name='subject'/>
+<input className='in emailInput' placeholder='Email' type="email" name='email' />
+<textarea className=' in msgInput' placeholder='Your Message' name="msg" id="" cols="20"  rows="10"></textarea>
+<button className='submitBtn' >Submit</button>
+
+
+
+</form>
+<AiOutlineClose onClick={resumec} className='close' />
+</div>
 
 </>
   )
